e6a388772dd5792471845cff10feb295b4652609
chocolatetl <41836645+chocolatetl@users.noreply.github.com>
2021-10-09 16:29:25 +0700

    Add product interface part 1

---
+++ eShop.Application/Catalog/Products/Dtos/ProductRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Application.Dtos
+{
+    public class ProductRequest
+    {
+
+    }
+}
---
+++ eShop.Application/Catalog/Products/IProductServices.cs
@@ -0,0 +1,16 @@
+﻿using eShop.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.Application.Catalog.Products
+{
+    public interface IProductServices
+    {
+        int Create(ProductRequest product);
+
+        int Update(ProductRequest product);
+
+        int Delete(int productId);
+    }
+}
